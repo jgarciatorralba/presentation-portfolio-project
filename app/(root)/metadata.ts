d6551cce39420a3cc1fd3421a38923995b2cfc72
Jorge García <jgarciatorralba@gmail.com>
2025-01-26 21:21:53 +0000

    replace favicon, update metadata

---
+++ app/(root)/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Jorge García",
+    description: "Jorge is a software engineer who enjoys building products for the web.",
+};

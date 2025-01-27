57979f564ca6294d07498263b8f18dbbf2bb5924
Jorge García <jgarciatorralba@gmail.com>
2025-01-27 20:36:39 +0000

    update fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         midnightBlue: "var(--midnight-blue)",
       },
+      fontFamily: {
+        roboto: "var(--font-roboto)",
+      }
     },
   },
   plugins: [],

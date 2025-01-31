e1d479367a3c257c69c24985cbcd5cf432fe88c0
Jorge García <jgarciatorralba@gmail.com>
2025-01-31 20:56:13 +0000

    style intro section

--- app/_components/intro.tsx
+++ app/_components/intro.tsx
@@ -3,9 +3,12 @@ import { ReactNode } from "react";
 export default function Intro(): ReactNode {
     return (
         <section>
-            <div className="p-12 lg:px-24 xl:px-32 2xl:px-64 lg:py-12 xl:py-16 2xl:py-32">
-                <p>Hey there!</p>
-                <p>I'm Jorge, a software engineer specializing in web applications.<br />Currently, I’m dedicated to enhancing one of the top travel booking platforms in Europe.</p>
+            <div className="h-screen flex items-center">
+                <div className="sm:px-12 md:px-24 lg:px-32 xl:px-64 py-12">
+                    <p className="mb-4">Hey there!</p>
+                    <p className="text-3xl sm:text-5xl mb-4"><span className="text-midnightBlue">I'm Jorge,</span> a software engineer specializing in web applications.</p>
+                    <p>Currently, I’m dedicated to enhancing one of the top travel booking platforms in Europe.</p>
+                </div>
             </div>
         </section>
     );

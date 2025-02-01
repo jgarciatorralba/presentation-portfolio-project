27122152cc8a5d95308f72916c8f3b5f8d59a332
Jorge García <jgarciatorralba@gmail.com>
2025-02-01 13:24:36 +0000

    reduce navbar height, remove right margin

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar(): ReactNode {
                         </li>
                     ))}
                 </ol>
-                <button id="btnSidebarToggler" type="button" className="mx-4 p-2 focus:outline rounded-xl md:hidden">
+                <button id="btnSidebarToggler" type="button" className="ml-4 p-2 focus:outline rounded-xl md:hidden">
                     <svg id="navClosed" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
                         stroke="currentColor" className="h-8 w-8">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />

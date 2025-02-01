8632263862d01d4f5f94013208ab880d39af55ff
Jorge García <jgarciatorralba@gmail.com>
2025-02-01 22:04:00 +0000

    add heading tags in intro

--- app/_components/intro.tsx
+++ app/_components/intro.tsx
@@ -6,8 +6,8 @@ export default function Intro(): ReactNode {
         <Section name="intro">
             <div className="h-screen flex items-center">
                 <div className="sm:px-12 md:px-24 lg:px-32 xl:px-64 py-12">
-                    <p className="mb-4">Hey there!</p>
-                    <p className="text-3xl sm:text-5xl mb-4"><span className="text-midnightBlue">I'm Jorge,</span> a software engineer specializing in web applications.</p>
+                    <h1 className="mb-4">Hey there!</h1>
+                    <h2 className="text-3xl sm:text-5xl mb-4"><span className="text-midnightBlue">I'm Jorge,</span> a software engineer specializing in web applications.</h2>
                     <p>Currently, I’m dedicated to enhancing one of the top travel booking platforms in Europe.</p>
                 </div>
             </div>

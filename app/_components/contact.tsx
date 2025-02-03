69472fa215dcc3b69f8d91247c6b15127ac88689
Jorge García <jgarciatorralba@gmail.com>
2025-02-03 18:49:55 +0000

    adjust extra margin in contact section

--- app/_components/contact.tsx
+++ app/_components/contact.tsx
@@ -8,7 +8,7 @@ export default function Contact(): ReactNode {
     return (
         <Section name="contact">
             <div>
-                <div className="section-container mb-12 sm:mb-24">
+                <div className="section-container mb-24 sm:mb-12">
                     <h2 className={`header ${oranienbaum.className}`}>Contact</h2>
                     <p className="mb-4">Feel free to reach out to discuss full-remote opportunities or any other coding-related topic, and I'll get back to you as soon as I can.</p>
                     <div className="flex justify-center py-8">

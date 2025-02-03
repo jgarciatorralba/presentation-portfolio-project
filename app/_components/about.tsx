4648f3696580759c14cd653bcac0a7f3a2a9b0f5
Jorge García <jgarciatorralba@gmail.com>
2025-02-03 20:32:12 +0000

    complete about section

--- app/_components/about.tsx
+++ app/_components/about.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { ReactNode } from "react";
 import { oranienbaum } from "../(root)/fonts";
 import Section from "./section";
@@ -8,18 +9,22 @@ export default function About(): ReactNode {
             <div>
                 <div className="section-container">
                     <h2 className={`header ${oranienbaum.className}`}>About</h2>
-                    <p className="paragraph">Hi, My name is Jorge and I develop digital products for a living. I've always been interested in technology for as long as I can remember, but it wasn't until 2020 that I decided to leave the automotive industry and try my luck with programming.
-                    </p>
-                    <p className="paragraph">Here are some technologies that I've worked with or that I'm eager to perfect at some point along my professional journey:</p>
-                    <ul>
-                        <li>PHP</li>
-                        <li>Symfony</li>
-                        <li>JavaScript</li>
-                        <li>TypeScript</li>
-                        <li>React</li>
-                        <li>Next.js</li>
-                        <li>Node.js</li>
-                    </ul>
+                    <div className="flex flex-col lg:flex-row items-center gap-12">
+                        <Image src="/pic.png" alt="Front Picture" width={300} height={300} className="rounded-md sepia-[.35] contrast-[1.1] border border-midnightBlue order-last lg:order-first w-[250px] md:w-[300px]" quality={100} />
+                        <div>
+                            <p className="paragraph">Hi, my name is Jorge and I develop digital products for a living. I've always been interested in technology for as long as I can remember, but it wasn't until 2020 that I decided to leave the automotive industry and try my luck with programming.
+                            </p>
+                            <p className="paragraph">Here are some technologies that I've worked with or that I'm eager to perfect at some point along my professional journey:</p>
+                            <ul className="list-[square] list-inside grid grid-cols-2 sm:grid-cols-[1fr_1fr_0.5fr]">
+                                <li className="sm:col-span-1">PHP</li>
+                                <li className="sm:col-span-2">Symfony</li>
+                                <li className="sm:col-span-1">JavaScript</li>
+                                <li className="sm:col-span-2">TypeScript</li>
+                                <li className="sm:col-span-1">React</li>
+                                <li className="sm:col-span-2">Next.js</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </Section>

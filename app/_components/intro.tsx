ecb1e61223de3e1e623460ea9cc89ad3b6f78fb9
Jorge García <jgarciatorralba@gmail.com>
2025-02-14 19:42:35 +0000

    adjust responsive padding

--- app/_components/intro.tsx
+++ app/_components/intro.tsx
@@ -6,7 +6,7 @@ export default function Intro(): JSX.Element {
     return (
         <Section name="intro">
             <div className="h-screen flex items-center">
-                <div className="sm:px-12 md:px-24 lg:px-32 xl:px-64 py-12">
+                <div className="sm:px-12 md:px-16 lg:px-32 xl:px-64 py-12">
                     <h1 className={`${oranienbaum.className} mb-4 text-2xl`}>Hey there!</h1>
                     <h2 className="text-3xl sm:text-5xl mb-4"><span className="text-midnightBlue">I'm Jorge,</span> a software engineer specializing in web applications.</h2>
                     <p className="paragraph">Currently, I’m dedicated to enhancing one of the top travel booking platforms in Europe.</p>

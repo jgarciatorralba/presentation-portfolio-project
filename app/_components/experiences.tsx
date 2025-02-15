fe07d8102a948bca0e5c9abb74f573c53a7e26d4
Jorge García <jgarciatorralba@gmail.com>
2025-02-15 17:45:21 +0000

    add second experience

--- app/_components/experiences.tsx
+++ app/_components/experiences.tsx
@@ -1,25 +1,38 @@
 import { JSX } from "react";
 
 const eDreamsUrl: string = "https://www.edreams.es/";
+const adkomoUrl: string = "https://www.adkomo.com/";
 
 const eDreams: JSX.Element = (
     <div className="tab-content">
         <h3 className="mb-4 text-lg md:text-2xl">Software Engineer @ <a className="text-midnightBlue" href={eDreamsUrl} target="_blank" rel="noopener noreferrer">eDreams ODIGEO</a></h3>
         <p className="paragraph">Working as a Backend Developer in the Accommodation area, contributing to the improvement of the hotel booking platform and post-booking services.</p>
         <ul className="list-disc list-inside pl-6">
-            <li className="-indent-[1.4rem]">Development of scalable solutions using PHP8, MariaDB, Redis and RabbitMQ.</li>
-            <li className="-indent-[1.4rem]">Active participation in product definition, applying Hexagonal Architecture and DDD.</li>
-            <li className="-indent-[1.4rem]">Implementation and maintenance of microservices, ensuring code quality and efficiency.</li>
-            <li className="-indent-[1.4rem]">Working in an agile environment (Kanban) with a focus on continuous value delivery.</li>
-            <li className="-indent-[1.4rem]">Monitoring and performance optimization using tools like Datadog and Grafana.</li>
+            <li className="indented-list-item">Development of scalable solutions using PHP8, MariaDB, Redis and RabbitMQ.</li>
+            <li className="indented-list-item">Active participation in product definition, applying Hexagonal Architecture and DDD.</li>
+            <li className="indented-list-item">Implementation and maintenance of microservices, ensuring code quality and efficiency.</li>
+            <li className="indented-list-item">Working in an agile environment (Kanban) with a focus on continuous value delivery.</li>
+            <li className="indented-list-item">Monitoring and performance optimization using tools like Datadog and Grafana.</li>
+        </ul>
+    </div>
+);
+
+const adkomo: JSX.Element = (
+    <div className="tab-content">
+        <h3 className="mb-4 text-lg md:text-2xl">Full Stack Developer @ <a className="text-midnightBlue" href={adkomoUrl} target="_blank" rel="noopener noreferrer">Adkomo</a></h3>
+        <p className="paragraph">Enhanced and maintained a proprietary advertising and digital marketing platform.</p>
+        <ul className="list-disc list-inside pl-6">
+            <li className="indented-list-item">Developed and optimized microservices architecture hosted on AWS.</li>
+            <li className="indented-list-item">Worked with Symfony (PHP 7.4), PostgreSQL, Redis, and React to build robust and efficient solutions.</li>
+            <li className="indented-list-item">Collaborated in an Agile Scrum environment to deliver high-quality products.</li>
         </ul>
     </div>
 );
 
 export default [
     { active: true, label: "eDreams", component: eDreams },
-    { active: false, label: "Adkomo", component: <div>Second experience</div> },
-    { active: false, label: "SII Concatel", component: <div>Third experience</div> },
-    { active: false, label: "Òmada Interactiva", component: <div>Fourth experience</div> },
-    { active: false, label: "Inbenta", component: <div>Fifth experience</div> },
+    { label: "Adkomo", component: adkomo },
+    { label: "SII Concatel", component: <div>Third experience</div> },
+    { label: "Òmada Interactiva", component: <div>Fourth experience</div> },
+    { label: "Inbenta", component: <div>Fifth experience</div> },
 ];

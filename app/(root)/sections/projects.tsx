61e564dc8aee32ee51581a7c225df436e4671778
Jorge García <jgarciatorralba@gmail.com>
2025-02-16 20:51:24 +0000

    first test fetching backend data

--- app/(root)/sections/projects.tsx
+++ app/(root)/sections/projects.tsx
@@ -2,12 +2,38 @@ import { JSX } from "react";
 import Section from "../../_components/section";
 import { oranienbaum } from "../fonts";
 
-export default function Projects(): JSX.Element {
+const API_URL: string = process.env.API_URL || '';
+const cacheLifetimeSeconds: number = 2 * 7 * 24 * 60 * 60;
+
+export default async function Projects(): Promise<JSX.Element> {
+    let projects: any[] = [];
+
+    try {
+        const response = await fetch(`${API_URL}/api/projects?pageSize=5`, { next: { revalidate: cacheLifetimeSeconds } });
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+        projects = data.projects;
+    } catch (error) {
+        console.error(error);
+    }
+
     return (
         <Section name="projects">
             <div className="section-container">
                 <h2 className={`header ${oranienbaum.className}`}>Projects</h2>
-                <p className="paragraph">Projects section.</p>
+                <p className="paragraph">A taste of my work.</p>
+                {projects && (
+                    <div className="projects">
+                        {projects.map((project: any) => (
+                            <p key={project.id}>{project.name}</p>
+                        ))}
+                    </div>
+                )}
             </div>
         </Section>
     );

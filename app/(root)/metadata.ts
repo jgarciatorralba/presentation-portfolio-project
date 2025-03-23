4dee204775b6ca713c6ed54e220a7acd7fd5bd1b
Jorge García <jgarciatorralba@gmail.com>
2025-03-23 18:56:00 +0000

    update metadata description

--- app/(root)/metadata.ts
+++ app/(root)/metadata.ts
@@ -2,5 +2,5 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
     title: "Jorge García",
-    description: "Jorge is a software engineer who enjoys building products for the web.",
+    description: "Jorge is a software engineer who enjoys building stuff for the web.",
 };

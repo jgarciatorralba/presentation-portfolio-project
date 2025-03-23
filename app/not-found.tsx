30db6622fb60d0d5ce57956d90dacb70213bac97
Jorge García <jgarciatorralba@gmail.com>
2025-03-23 21:20:13 +0000

    WIP: not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from 'next/link';
+import { JSX } from "react";
+import { oranienbaum, roboto } from "./fonts";
+import "./styles/globals.css";
+
+export default function NotFound(): JSX.Element {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen text-center px-6">
+            <h1 className={`${oranienbaum.className} header`}>Not Found</h1>
+            <p className={`${roboto.className} paragraph`}>
+                Nothing to see here... Shall we try again?<br />
+                To <Link className="paragraph font-bold" href="/">home</Link> page.
+            </p>
+            <Image
+                src="/not-found.svg"
+                alt="Not Found"
+                width={250}
+                height={250}
+                quality={100}
+                priority
+            />
+        </div >
+    );
+}

ed22788e0791922f4a9154a2ba8f3836c8be0415
Jorge García <jgarciatorralba@gmail.com>
2025-04-13 19:23:08 +0000

    add links and icons to project card

--- app/_components/sections/projects/projectCard.tsx
+++ app/_components/sections/projects/projectCard.tsx
@@ -1,11 +1,48 @@
+import Image from "next/image";
 import { ProjectCardProps } from "projects";
 import { JSX } from "react";
 
 export default function ProjectCard({ name, description, topics, repository, homepage }: ProjectCardProps): JSX.Element {
     return (
-        <div className="bg-blue hover:bg-midnight-blue rounded-md p-6 transition hover:scale-105">
-            <p className="paragraph capitalize text-lg font-bold">{name.replaceAll('-', ' ')}</p>
-            <p className="paragraph text-sm">{description}</p>
+        <div className="bg-blue hover:bg-midnight-blue rounded-md p-6 transition hover:scale-105 flex flex-col justify-between">
+            <div>
+                <div className="flex flex-row justify-end mb-6 sm:mb-4">
+                    <a
+                        className="hover:cursor-pointer ml-1"
+                        href={repository}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                    >
+                        <Image
+                            src={"/github.svg"}
+                            quality={100}
+                            alt={"GitHub Logo"}
+                            width={30}
+                            height={30}
+                        />
+                    </a>
+                    {homepage && (
+                        <a
+                            className="hover:cursor-pointer ml-1"
+                            href={homepage}
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            <Image
+                                src={"/external-link.svg"}
+                                quality={100}
+                                alt={"External Link"}
+                                width={30}
+                                height={30}
+                            />
+                        </a>
+                    )}
+                </div>
+                <div>
+                    <p className="paragraph capitalize text-lg font-bold">{name.replaceAll('-', ' ')}</p>
+                    <p className="paragraph text-sm">{description}</p>
+                </div>
+            </div>
 
             <div className="flex flex-wrap gap-2">
                 {topics.map((topic, index) => (

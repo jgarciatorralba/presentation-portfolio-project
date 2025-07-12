7c2c2e56187f188eccbb6dbe19b146c78b98ae41
Jorge García <jgarciatorralba@gmail.com>
2025-07-12 17:22:36 +0000

    complete unit tests for section "about"

--- tests/unit/__tests__/(root)/sections/about.test.tsx
+++ tests/unit/__tests__/(root)/sections/about.test.tsx
@@ -3,20 +3,33 @@ import { render, screen } from "@testing-library/react";
 import About from "../../../../../app/(root)/sections/about";
 
 describe("About component", () => {
-    it("Renders the heading About", () => {
+    it("Renders the heading \"About\"", () => {
         render(<About />);
-        expect(screen.getByRole("heading", { name: /about/i })).toBeInTheDocument();
+
+        expect(screen.getByRole("heading", { name: /About/ })).toBeInTheDocument();
     });
 
     it("Shows the main image", () => {
         const { container } = render(<About />);
         const img = container.querySelector('img[alt="Front Picture"]');
+
         expect(img).toBeInTheDocument();
     });
 
     it("Shows the social media links", () => {
         render(<About />);
         const links = screen.getAllByRole("link");
-        expect(links.length).toBeGreaterThan(0);
+
+        expect(links.length).toBe(2);
+        links.forEach(link => {
+            expect(link).toHaveAttribute("rel", "noopener noreferrer");
+            expect(link).toHaveAttribute("target", "_blank");
+        });
+
+        const socialIcons = screen.getAllByRole('img', { name: /Logo/ });
+
+        expect(socialIcons.length).toBe(2);
+        expect(socialIcons[0]).toHaveAttribute("alt", "GitHub Logo");
+        expect(socialIcons[1]).toHaveAttribute("alt", "LinkedIn Logo");
     });
 });

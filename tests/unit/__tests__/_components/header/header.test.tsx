39f2859ef70ec740e5f90e8bfa8d1f073df57057
Jorge García <jgarciatorralba@gmail.com>
2025-07-26 08:20:58 +0000

    complete unit tests in header component

--- tests/unit/__tests__/_components/header/header.test.tsx
+++ tests/unit/__tests__/_components/header/header.test.tsx
@@ -8,11 +8,13 @@ describe("Header component", () => {
         const { container } = render(<Header />);
 
         const headerElement: HTMLElement | null = container.querySelector("header");
+
         expect(headerElement).not.toBeNull();
         expect(headerElement).toBeInTheDocument();
 
-        const navbarElement = screen.getByRole("navigation");
-        expect(navbarElement).toBeInTheDocument();
+        const navbarElementMobile = screen.getByRole("navigation");
+
+        expect(navbarElementMobile).toBeInTheDocument();
     });
 
     it("Applies \"scrolled\" class when window is scrolled", () => {
@@ -43,5 +45,68 @@ describe("Header component", () => {
 
         expect(headerElement).not.toBeNull();
         expect(headerElement).not.toHaveClass("scrolled");
+    });
+
+    it("Contains all expected links in the navbar", () => {
+        const { container } = render(<Header />);
+
+        const navbarElementMobile: HTMLElement | null = container.querySelector("nav > div");
+        const navbarElementDesktop: HTMLElement | null = container.querySelector("nav > div:nth-child(2)");
+
+        [navbarElementMobile, navbarElementDesktop].forEach(navbarElement => {
+            expect(navbarElement).not.toBeNull();
+
+            if (navbarElement !== null) {
+                const links = navbarElement.querySelectorAll("a");
+
+                expect(links.length).toBe(5);
+                expect(links[0]).toHaveTextContent("About");
+                expect(links[1]).toHaveTextContent("Experience");
+                expect(links[2]).toHaveTextContent("Projects");
+                expect(links[3]).toHaveTextContent("Contact");
+                expect(links[4]).toHaveTextContent("Resume");
+            }
+        });
+    });
+
+    it("Downloads the resume when the Resume link is clicked", () => {
+        const { container } = render(<Header />);
+
+        const resumeLink: HTMLElement | null = container.querySelector("a[href='/resume.pdf']");
+
+        expect(resumeLink).not.toBeNull();
+
+        if (resumeLink !== null) {
+            expect(resumeLink).toHaveAttribute("target", "_blank");
+            expect(resumeLink).toHaveAttribute("rel", "noopener noreferrer");
+            expect(resumeLink).toHaveTextContent("Resume");
+
+        }
+    });
+
+    it("Toggles the sidebar when NavbarButton is clicked", () => {
+        const { container } = render(<Header />);
+
+        const navbarButton: HTMLElement | null = container.querySelector("button.navButton");
+
+        expect(navbarButton).not.toBeNull();
+
+        if (navbarButton !== null) {
+            // Click the button to open the sidebar
+            act(() => {
+                navbarButton.click();
+            });
+
+            const sidebarElement = container.querySelector(".containerSidebar");
+
+            expect(sidebarElement).toHaveClass("open");
+
+            // Click the button again to close the sidebar
+            act(() => {
+                navbarButton.click();
+            });
+
+            expect(sidebarElement).not.toHaveClass("open");
+        }
     });
 });

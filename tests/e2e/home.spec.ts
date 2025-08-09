69eb0bda7280854cdfdd882bc2d184d4f30dce63
Jorge García <jgarciatorralba@gmail.com>
2025-08-09 17:44:15 +0000

    use experimental test mode to mock server-side requests

--- tests/e2e/home.spec.ts
+++ tests/e2e/home.spec.ts
@@ -1,7 +1,21 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "next/experimental/testmode/playwright";
 import { navigation } from '../../app/_lib/constants';
+import { sampleProject } from "../sampleProject";
 
 test.describe('Home Page', () => {
+  test.beforeEach(async ({ next }) => {
+    next.onFetch((request) => {
+      if (request.url.includes('projects?pageSize=6')) {
+        return new Response(JSON.stringify({
+          projects: [sampleProject],
+          count: 1
+        }));
+      }
+
+      return "abort";
+    });
+  });
+
   test('Shows all sections', async ({ page }) => {
     await page.goto('http://localhost:3000/');
 
@@ -16,12 +30,8 @@ test.describe('Home Page', () => {
   test('Shows pre-fetched projects', async ({ page }) => {
     await page.goto('http://localhost:3000/');
 
-    // Expect a title "to contain" a substring.
-    // await expect(page).toHaveTitle(/Playwright/);
-
-    const heading = page.getByRole('heading', { level: 1 });
-    await expect(heading).toBeVisible();
-    await expect(heading).toHaveText('Hey there!');
+    const project = page.locator('p.text-lg.font-bold', { hasText: sampleProject.name });
+    await expect(project).toBeVisible();
   });
 
   test('Shows more projects when clicking "Show more" button', async ({ page }) => {
@@ -46,11 +56,18 @@ test.describe('Home Page', () => {
     await expect(heading).toHaveText('Hey there!');
   });
 
-  test('Hides "Projects" section when there are no projects', async ({ page }) => {
+  test('Hides "Projects" section when there are no projects', async ({ page, next }) => {
+    next.onFetch((request) => {
+      if (request.url.includes('projects?pageSize=6')) {
+        return new Response('Internal Server Error', { status: 500 });
+      }
+
+      return "abort";
+    });
+
     await page.goto('http://localhost:3000/');
 
-    const heading = page.getByRole('heading', { level: 1 });
-    await expect(heading).toBeVisible();
-    await expect(heading).toHaveText('Hey there!');
+    const heading = page.getByRole('heading', { name: 'Projects' });
+    await expect(heading).not.toBeVisible();
   });
 });

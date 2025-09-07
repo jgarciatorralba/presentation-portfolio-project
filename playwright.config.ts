1c8e00e38c8943b2e8335c49d186d25b45c40a1b
Jorge García <jgarciatorralba@gmail.com>
2025-09-07 15:54:47 +0000

    remove blank spaces

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,28 +32,23 @@ export default defineConfig({
   use: {
     trace: 'on-first-retry',
   },
-
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
     {
       name: 'Mobile Chrome',
       use: { ...devices['Pixel 5'] },
     },
-
     {
       name: 'Mobile Safari',
       use: { ...devices['iPhone 15 Pro'] },
